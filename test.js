eded8846fa652e829cf1660c20970619f872e1f7
Rafael Gonzaga <rafael.nunu@hotmail.com>
2023-03-24 20:56:22 -0300

    fix: add check when invalid argument (#1)
    * fix: add test compare options
    
    * fix: add check when invalid argument
    
    * tests: add a test to the fixed behavior

--- test.js
+++ test.js
@@ -4,4 +4,11 @@ const { sum } = require('.')
 
 test('sum correctly', () => {
   assert.strictEqual(sum(1, 1), 2);
+  assert.strictEqual(sum(1, 2), 3);
+})
+
+test('handle invalid argument', () => {
+  assert.throws(() => {
+    sum('no', 'no')
+  }, { message: 'The argument must be a number' })
 })
