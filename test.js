d32aea9510c1edb2aadb837263c6d48960459c9b
RafaelGSS <rafael.nunu@hotmail.com>
2023-03-25 00:49:59 +0100

    tests: add a test to the fixed behavior

--- test.js
+++ test.js
@@ -6,3 +6,9 @@ test('sum correctly', () => {
   assert.strictEqual(sum(1, 1), 2);
   assert.strictEqual(sum(1, 2), 3);
 })
+
+test('handle invalid argument', () => {
+  assert.throws(() => {
+    sum('no', 'no')
+  }, { message: 'The argument must be a number' })
+})
