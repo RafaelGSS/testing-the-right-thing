4fdc20acf7126a2f36f31b5454a94caf3b3d7018
RafaelGSS <rafael.nunu@hotmail.com>
2023-03-28 17:08:01 +0100

    fix: supporting number as string

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function sum (a, b) {
+if (typeof a === 'string' && +a !== NaN) {
+    a = +a
+  }
+
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new Error('The argument must be a number')
   }
